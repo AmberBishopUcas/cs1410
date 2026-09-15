312622bbc815fcb8bac39f50a019d9fb3592e78a
MarkBishopUcas <mark.bishop@ucas-edu.net>
2026-09-15 20:48:31 +0000

    started BPE file

--- final_project/byte_pair_encoding.py
+++ final_project/byte_pair_encoding.py
@@ -11,21 +11,36 @@ import csv
 import os
 
 wrdlst_filename = os.path.join(os.path.dirname(__file__), "wordlist.csv")
-folder = "training_data"
+folder = os.path.join(os.path.dirname(__file__), "training_data")
 
-def byte_pair_vocab_maker():
-    wordlst = []
-    with open(wrdlst_filename, "r") as file:
-        reader = csv.reader(file)
-        next(reader)
-        for row in reader:
-            wordlst.append(row[1])
+def load_text_as_tokens(folder):
+    corpus_tokens = []
     for filename in os.listdir(folder):
         if filename.endswith(".txt") and filename != "README.txt":
             file_path = os.path.join(folder, filename)
-
             with open(file_path, "r", encoding="utf-8") as file:
-                text = file.read()
-                print(f"Processing file: {filename}")
+                # Convert "hello" into ['h', 'e', 'l', 'l', 'o']
+                file_tokens = list(file.read())
+                corpus_tokens.append(file_tokens)
+    return corpus_tokens
 
-byte_pair_vocab_maker()
+
+
+def count_byte_pairs(all_files_tokens):
+    word_pairs = {}
+
+    # Iterate through each file's token list
+    for file_tokens in all_files_tokens:
+        # Loop stops 1 item short so i+1 always exists
+        for i in range(len(file_tokens) - 1):
+            
+            # Skip spaces so we don't pair words together
+            if file_tokens[i] != " " and file_tokens[i+1] != " ":
+                # Create a tuple pair like ('a', 'b') or ('ab', 'c')
+                pair = (file_tokens[i], file_tokens[i+1])
+                
+                if pair in word_pairs:
+                    word_pairs[pair] += 1
+                else:
+                    word_pairs[pair] = 1
+    return word_pairs
